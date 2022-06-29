ac275c13819d202af5ac5898b5280c515badf949
Adnan-kreiker <adnankreiker@hotmail.com>
2022-06-29 23:37:11 +0200

    chore: add gitignore

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { themeChange } from 'theme-change'
+
+
+onMounted(() => {
+  themeChange(false)
+})
+</script>
+
+<template>
+<div class="dark:bg-zinc-600 h-screen">
+    <div class="navbar bg-base-100">
+  <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+</div>
+  <button data-toggle-theme="dark,light" data-act-class="ACTIVECLASS">Change</button>
+  <slot />
+</div>
+</template>
