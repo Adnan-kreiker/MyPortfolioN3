4bee2aa88a0087eb4371a2121127c8e5eebb2980
Adnan-kreiker <adnankreiker@hotmail.com>
2022-06-30 11:18:35 +0200

    config: add linting

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import { themeChange } from 'theme-change'
 
-
 onMounted(() => {
   themeChange(false)
 })
 </script>
 
 <template>
-<div class="dark:bg-zinc-600 h-screen">
+  <div class="dark:bg-zinc-600 h-screen">
     <div class="navbar bg-base-100">
-  <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-</div>
-  <button data-toggle-theme="dark,light" data-act-class="ACTIVECLASS">Change</button>
-  <slot />
-</div>
+      <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+    </div>
+
+    <button class="ml-auto absolute top-6 right-0 mr-2" data-toggle-theme="dark,light" data-act-class="ACTIVECLASS">
+      <input type="checkbox" class="toggle toggle-sm" checked>
+    </button>
+    <slot />
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,19 @@
 import { defineNuxtConfig } from 'nuxt'
-import TypedRouter from 'nuxt-typed-router';
+import TypedRouter from 'nuxt-typed-router'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   buildModules: ['@nuxtjs/tailwindcss', TypedRouter],
   modules: [
-    '@vueuse/nuxt',
+    '@vueuse/nuxt'
   ],
   css: [
-    '~/assets/css/main.css',
+    '~/assets/css/main.css'
   ],
+  vite: {
+    plugins: [
+      eslintPlugin()
+    ]
+  }
 })
